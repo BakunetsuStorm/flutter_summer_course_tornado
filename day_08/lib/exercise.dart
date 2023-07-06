80f4ede79c64d2dae598cfe90e9c04a68f099bc2
Tornado-8 <tornado-8@Admin-8s-iMac.local>
2023-07-06 17:37:21 +0800

    exercise

---
+++ day_08/lib/exercise.dart
@@ -0,0 +1,17 @@
+void main() {
+  print('day_08');
+  print(avg(3, 5));
+  print(nuatbutsaalt(3.14, 9.81, 6.9));
+  ailaif('asdasfsf','gdfashdf' );
+}
+
+double avg(int a, int b) {
+  return (a + b) / 2;
+}
+double nuatbutsaalt(double a, double b, double c){
+  return a*b*c;
+}
+void ailaif (String asd, String qwerty){
+  print('$asd $qwerty');
+
+}
