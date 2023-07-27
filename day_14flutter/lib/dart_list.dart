45fb202c71e17ecc8733cf32c3831bc19d91cbc8
BakunetsuStorm <temuulenzolzaya889@gmail.com>
2023-07-27 17:10:48 +0800

    add:day_14

---
+++ day_14flutter/lib/dart_list.dart
@@ -0,0 +1,7 @@
+
+var numberList = [1, 2, 3, 5, 7, 'Useg'];
+List<String> stringList = ['S', 'H', 'I', 'T'];
+ void main(){
+   print(numberList);
+   print(stringList);
+ }
---
+++ day_14flutter/lib/profile_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          _buildProfileImage(context),
+          _buildProfileDetails(context),
+          _buildActions(context)
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileImage(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      child: ClipOval(
+        child: Image.asset(
+          'assets/ygbi.jpg',
+          fit: BoxFit.fitHeight,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProfileDetails(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Just Ken',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+            ),
+          _buildDetailRow('Age', '18'),
+          _buildDetailRow('Status', ('Kenough')),
+            //_buildActions(context),
+          ],
+        ));
+  }
+
+  // Widget _buildActions(BuildContext context) {
+  //   return Container();
+  // }
+  Widget _buildDetailRow(String heading, String value ){
+    return Row(
+      children: [
+        Text('$heading ', style: TextStyle(fontWeight: FontWeight.bold)),
+
+
+        Text('$value')
+
+      ],
+    );
+  }
+  Widget _buildActions(BuildContext context){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildIcon(Icons.restaurant, 'Feed'),
+        _buildIcon(Icons.favorite, 'Barbie'),
+        _buildIcon(Icons.card_travel, 'Mojo Dojo Casa'),
+      ],
+    );
+  }
+  Widget _buildIcon(IconData icon, String text){
+    return Padding(padding: EdgeInsets.all(20),
+    child: Column(
+      children: [
+        Icon(icon, size: 40,),
+        Text(text),
+      ],
+    ),);
+  }
+}
