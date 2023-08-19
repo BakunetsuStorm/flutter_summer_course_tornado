46a49c30afc2dae99db22f3cc38fcb029ef01595
BakunetsuStorm <temuulenzolzaya889@gmail.com>
2023-08-19 17:29:19 +0800

    icodegram_app

--- icodegram_app/lib/main.dart
+++ icodegram_app/lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:icodegram_app/resources/auth_methods.dart';
+import 'package:icodegram_app/text_input_field.dart';
+import 'package:icodegram_app/pages/login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
-    title: "Twicetagram",
-    theme: ThemeData(fontFamily: 'Lobster'),
-    home: const login,
-  ), );
+    debugShowCheckedModeBanner: false,
+    title: 'Twicetagram',
+    theme: ThemeData.dark().copyWith(
+      scaffoldBackgroundColor: Color.fromRGBO(0, 0, 0, 1),
+      textTheme: Typography().white.apply(fontFamily: 'Rubik'),
+    ),
+    home: LoginScreen(),
+  ));
 }
--- icodegram_app/lib/pages/login.dart
+++ icodegram_app/lib/pages/login.dart
@@ -1,15 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:icodegram_app/resources/auth_methods.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+import '../home_screen.dart';
+import '../text_input_field.dart';
+import 'home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
+  void loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    String result = await AuthMethods().loginUser(
+        email: _emailController.text, password: _passwordController.text);
+
+    if (result == 'success') {
+      setState(() {
+        _isLoading = false;
+      });
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomeScreen()));
+      print('Logged in');
+    } else {
+      print("Not logged in");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ;
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(flex: 2, child: Container()),
+              Text('Twicetagram'),
+              SizedBox(height: 64),
+              TextInputField(
+                hintText: 'Имэйл эсвэл хэрэглэгчийн нэр',
+                isPassword: false,
+                editingController: _emailController,
+                textInputType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 24),
+              TextInputField(
+                  hintText: 'Нууц үг',
+                  isPassword: true,
+                  editingController: _passwordController,
+                  textInputType: TextInputType.text),
+              SizedBox(height: 64),
+              InkWell(
+                onTap: loginUser,
+                child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(vertical: 12),
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4))),
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  child: _isLoading
+                      ? Center(
+                          child: CircularProgressIndicator(color: Colors.white))
+                      : Text('Нэвтрэх'),
+                ),
+              ),
+              SizedBox(height: 12),
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
+              Text("Эсвэл",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  )),
+              SizedBox(height: 12),
+              Flexible(child: Container(), flex: 4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Шинэ хэрэглэгч үү?  ",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Text(
+                    "Бүртгүүлэх",
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.orange),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Flexible(
+                    child: Container(),
+                    flex: 2,
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
